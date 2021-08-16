445d7d9f4d48c8be5a0e5b6f709dcd771cb6323c
Anjo <anjojy@gmail.com>
2021-08-16 11:54:57 +0530

    API services
    Api services

--- lib/Model/ProductDetails.dart
+++ lib/Model/ProductDetails.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+ProductDetailModel advertisementFromJson(String str) =>
+    ProductDetailModel.fromJson(json.decode(str));
+
 class ProductDetailModel {
   final String productimg, name, price, colorimg;
   final int id, ram, storage, battery, camera, display;
@@ -13,4 +18,18 @@ class ProductDetailModel {
     required this.camera,
     required this.display,
   });
+
+  factory ProductDetailModel.fromJson(Map<String, dynamic> json) =>
+      ProductDetailModel(
+        name: json["name"] == null ? null : json["name"],
+        price: json["price"] == null ? null : json["price"],
+        id: json["id"] == null ? null : json["id"],
+        ram: json["ram"] == null ? null : json["ram"],
+        productimg: ["image"] == null ? null : json["productimg"],
+        colorimg: json["color"] == null ? null : json["colorimg"],
+        storage: json["storage"] == null ? null : json["storage"],
+        battery: json["battery"] == null ? null : json["battery"],
+        camera: json["camera"] == null ? null : json["camera"],
+        display: json["display"] == null ? null : json["display"],
+      );
 }
